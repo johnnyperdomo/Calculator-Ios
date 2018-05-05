e4966a7514a89e99b51fb04701b5a38373ec6b21
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-05-05 14:20:29 -0400

    build calculator View Controller

---
+++ Calculator(ios)/Controller/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Calculator(ios)
+//
+//  Created by Johnny Perdomo on 5/5/18.
+//  Copyright © 2018 Johnny Perdomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var numberView: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func clearBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func dotBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func numberPressed(_ sender: Any) {
+    }
+    
+    @IBAction func multiplicationBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func divisionBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func subtractionBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func additionBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func equalBtnPressed(_ sender: Any) {
+    }
+    
+    
+}
+
